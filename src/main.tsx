384e7264d76e84851514d83cfa769e25fa805dd4
Steve Xu <stevexugc@outlook.com>
2026-01-16 16:34:41 +0800

    feat: add sentry

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,28 @@ import "./App.css";
 import i18n from "./i18n";
 import { useSettingStore } from "./store";
 import { isFullScreen } from "./util";
+import {
+  init as initSentry,
+  browserTracingIntegration,
+  replayIntegration,
+} from "@sentry/react";
+
+if (location.hostname === "nusr.github.io") {
+  initSentry({
+    dsn: "https://07d089a03337af8aee98719f34a3164a@o4506851168092160.ingest.us.sentry.io/4510718843420672",
+    integrations: [
+      browserTracingIntegration(),
+      replayIntegration({
+        maskAllText: false,
+        blockAllMedia: false,
+      }),
+    ],
+    tracesSampleRate: 1.0,
+    tracePropagationTargets: ["nusr.github.io"],
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0,
+  });
+}
 
 async function init() {
   await i18n.changeLanguage();
