cf248148137cf2eb31345550ed0ecf6aa8e59876
Steve Xu <stevexugc@outlook.com>
2026-01-19 14:29:20 +0800

    fix: CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: 5,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [["github"], ["html"]],
+  reporter: [["github"], ["html"], ["list"]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
