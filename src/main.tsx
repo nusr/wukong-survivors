833e1fea7b33bb5024e1706f5dd8602052550ff1
Steve Xu <stevexugc@outlook.com>
2026-09-11 10:05:20 +0800

    fix: update sentry

--- src/main.tsx
+++ src/main.tsx
@@ -5,38 +5,7 @@ import "./App.css";
 import i18n from "./i18n";
 import { useSettingStore } from "./store";
 import { isFullScreen } from "./util";
-import {
-  init as initSentry,
-  browserTracingIntegration,
-  replayIntegration,
-} from "@sentry/react";
-
-declare global {
-  interface Window {
-    __bundle_info?: {
-      time?: string;
-      commit_id?: string;
-    };
-  }
-}
-
-if (location.hostname === "nusr.github.io") {
-  initSentry({
-    dsn: "https://07d089a03337af8aee98719f34a3164a@o4506851168092160.ingest.us.sentry.io/4510718843420672",
-    integrations: [
-      browserTracingIntegration(),
-      replayIntegration({
-        maskAllText: false,
-        blockAllMedia: false,
-      }),
-    ],
-    tracesSampleRate: 1.0,
-    tracePropagationTargets: ["nusr.github.io"],
-    replaysSessionSampleRate: 0.1,
-    replaysOnErrorSampleRate: 1.0,
-    release: window.__bundle_info?.commit_id,
-  });
-}
+import './sentry'
 
 async function init() {
   await i18n.changeLanguage();
---
+++ src/sentry.ts
@@ -0,0 +1,45 @@
+import * as Sentry from "@sentry/react";
+
+declare global {
+  interface Window {
+    __bundle_info?: {
+      time?: string;
+      commit_id?: string;
+    };
+  }
+}
+
+if (
+  typeof window !== "undefined" &&
+  window.location.hostname === "nusr.github.io" &&
+  !Sentry.isInitialized()
+) {
+  Sentry.init({
+    dsn: "https://b292d91ba509038c141ecfc7d10e7bb7@o4506851168092160.ingest.us.sentry.io/4506851171041280",
+    integrations: [
+      // If you're using react router, use the integration for your react router version instead.
+      // Learn more at
+      // https://docs.sentry.io/platforms/javascript/guides/react/configuration/integrations/react-router/
+      Sentry.browserTracingIntegration(),
+      Sentry.replayIntegration(),
+      Sentry.feedbackIntegration({
+        // Additional SDK configuration goes in here, for example:
+        colorScheme: "system",
+      }),
+    ],
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for tracing.
+    // Learn more at
+    // https://docs.sentry.io/platforms/javascript/configuration/options/#traces-sample-rate
+    tracesSampleRate: 1.0,
+    // Set `tracePropagationTargets` to control for which URLs trace propagation should be enabled
+    tracePropagationTargets: [/^https:\/\/nusr\.github\.io/],
+    // Capture Replay for 10% of all sessions,
+    // plus for 100% of sessions with an error
+    // Learn more at
+    // https://docs.sentry.io/platforms/javascript/session-replay/configuration/#general-integration-configuration
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0,
+    release: window.__bundle_info?.commit_id,
+  });
+}
